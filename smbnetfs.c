f97aaddf63ca97a6dda0573ac9971118ed6a3139
Giuseppe Scrivano <gscrivano@gnu.org>
2006-04-13 16:22:42 +0000

    Updated email address

--- smbnetfs.c
+++ smbnetfs.c
@@ -1,7 +1,7 @@
 /*
   Copyright (C) 1997, 2002, 2004 Free Software Foundation, Inc.
   Copyright (C) 2004 Giuseppe Scrivano.
-  Written by Giuseppe Scrivano <gscrivano@quipo.it>
+  Written by Giuseppe Scrivano <gscrivano@gnu.org>
 
   This program is free software; you can redistribute it and/or
   modify it under the terms of the GNU General Public License as
